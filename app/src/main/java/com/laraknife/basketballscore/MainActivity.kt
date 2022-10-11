0462504cea7c6a6ac75a8b5e2adc677fcbf46953
LaraKnife <192t0220@itsm.edu.mx>
2022-10-11 11:14:18 -0500

    git commit

--- app/src/main/java/com/laraknife/basketballscore/MainActivity.kt
+++ app/src/main/java/com/laraknife/basketballscore/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.laraknife.basketballscore
 
+import com.laraknife.basketballscore.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Tareas a Realizar //
+
     }
 }
+
+
+/* override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calcular.setOnClickListener {
+            val edadString = binding.edad.text.toString()
+            if (edadString.isEmpty()){
+                Toast.makeText(this,"Ingrese una edad",Toast.LENGTH_LONG).show()
+            }else{
+                val age = edadString.toInt()
+                val edaddog = age * 7
+                binding.resultado.text = "Su edad es años perro sería $edaddog"
+            }
+        }
+    }
+ */
--- app/src/main/java/com/laraknife/basketballscore/ScoreActivity.kt
+++ app/src/main/java/com/laraknife/basketballscore/ScoreActivity.kt
@@ -2,10 +2,15 @@ package com.laraknife.basketballscore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.laraknife.basketballscore.databinding.ActivityScoreBinding
 
 class ScoreActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_score)
+        val binding = ActivityScoreBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Tareas a Realizar //
+
     }
 }
